241a6c58b46e9fcc8bd12105a3a2b2eee2defa3a
emilpoisdown-dev <emilpoisdown@gmail.com>
2026-06-17 18:14:35 -0400

    Commit v7

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'elders-ga-guide-v2';
+const CACHE = 'elders-ga-guide-v7';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
